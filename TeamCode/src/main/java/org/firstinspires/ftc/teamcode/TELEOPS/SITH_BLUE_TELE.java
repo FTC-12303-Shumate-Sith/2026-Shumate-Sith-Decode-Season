6ed4837d8a6e9a937299ccb82ce2c896a57ff954
Daniel Romanski <daniellromanski@yahoo.com>
2026-04-06 16:00:44 -0400

    Basic Tuning

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TELEOPS/SITH_BLUE_TELE.java
@@ -0,0 +1,140 @@
+package org.firstinspires.ftc.teamcode.TELEOPS;
+
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.HeadingInterpolator;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.CRServoImpl;
+import com.qualcomm.robotcore.hardware.CRServoImplEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+import java.util.function.Supplier;
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// OP MODE & CONFIGURATION
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+@Configurable
+@TeleOp
+public class SITH_BLUE_TELE extends OpMode {
+
+    private Follower follower;
+    public static Pose startingPose;
+    private boolean automatedDrive;
+    private Supplier<PathChain> pathChain;
+    private TelemetryManager telemetryM;
+    private boolean slowMode = false;
+    private double slowModeMultiplier;
+
+    private CRServo testServo = null;
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// INIT
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    @Override
+    public void init() {
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startingPose == null ? new Pose() : startingPose);
+        follower.update();
+        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+
+        pathChain = () -> follower.pathBuilder()
+                .addPath(new Path(new BezierLine(follower::getPose, new Pose(45, 98))))
+                .setHeadingInterpolation(HeadingInterpolator.linearFromPoint(follower::getHeading, Math.toRadians(45), 0.8))
+                .build();
+
+
+        //-------------------
+        //----INIT SERVOS----
+        //-------------------
+        testServo = hardwareMap.get(CRServo.class, "Servo");
+        testServo.setPower(0.0);
+
+    }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Start
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+   @Override
+   public void start() {
+        follower.startTeleopDrive();
+
+  }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Loop
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+   @Override
+   public void loop() {
+
+       //Call this once per loop
+       follower.update();
+       telemetryM.update();
+       if (!automatedDrive) {
+           //Make the last parameter false for field-centric
+           //In case the drivers want to use a "slowMode" you can scale the vectors
+           //This is the normal version to use in the TeleOp
+           if (!slowMode) follower.setTeleOpDrive(
+                   -gamepad1.left_stick_y,
+                   -gamepad1.left_stick_x,
+                   -gamepad1.right_stick_x,
+                   true // Robot Centric
+           );
+               //This is how it looks with slowMode on
+           else follower.setTeleOpDrive(
+                   -gamepad1.left_stick_y * slowModeMultiplier,
+                   -gamepad1.left_stick_x * slowModeMultiplier,
+                   -gamepad1.right_stick_x * slowModeMultiplier,
+                   true // Robot Centric
+           );
+       }
+       //Automated PathFollowing
+       if (gamepad1.aWasPressed()) {
+           follower.followPath(pathChain.get());
+           automatedDrive = true;
+       }
+       //Stop automated following if the follower is done
+       if (automatedDrive && (gamepad1.bWasPressed() || !follower.isBusy())) {
+           follower.startTeleopDrive();
+           automatedDrive = false;
+       }
+       //Slow Mode
+       if (gamepad1.rightBumperWasPressed()) {
+           slowMode = !slowMode;
+       }
+       //Optional way to change slow mode strength
+       if (gamepad1.xWasPressed()) {
+           slowModeMultiplier += 0.25;
+       }
+       //Optional way to change slow mode strength
+       if (gamepad2.yWasPressed()) {
+           slowModeMultiplier -= 0.25;
+       }
+
+
+       if (gamepad1.a) {
+           testServo.setPower(1.0);
+       } else if (gamepad1.b) {
+           testServo.setPower(-1.0);
+       } else {
+           testServo.setPower(0.0);
+       }
+
+
+       telemetryM.debug("position", follower.getPose());
+       telemetryM.debug("velocity", follower.getVelocity());
+       telemetryM.debug("automatedDrive", automatedDrive);
+    }
+}
